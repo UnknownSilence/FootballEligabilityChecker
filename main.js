50b41f4039889fb2c1ee9ace432b6c18426f53da
Trent Hardy <TrentThomasHardy@yahoo.com>
2019-08-27 13:38:27 -0500

    code structure updates and more tests. remote.

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
 
 
-var satScore
-var actScore
+var satScore = 1250 //test
+var actScore = 24 // test
 
-var scienceSem1Grades = [90, 82, 99, 100]
-var scienceSem2Grades = [60, 80, 89, 91]
+var scienceSem1Grades = [90, 82, 99, 100] //test
+var scienceSem2Grades = [60, 80, 89, 91] //test
 
-var eligable = false
+var englishSem1Grades = [98, 78, 85, 90] //test
+var englishSem2Grades = [90, 87, 76, 99] //test
 
-checkEligability(satScore, actScore, scienceSem1Grades, scienceSem2Grades)
+var otherSem1Grades = [99, 96, 90, 100] //test
+var otherSem2Grades = [89, 92, 98, 86] //test
+
+var mathSem1Grades = [90, 98, 87] //test
+var mathSem2Grades = [90, 87, 76] //test
+
+
+
+
+var eligable = false // initialize value at default of false until proven true
+
+document.addEventListener('load', function() {
+    if (checkEligability(satScore, actScore, scienceSem1Grades, scienceSem2Grades) == true) {
+        let label = document.getElementById('eligableLabel')
+        label.textContent = "ELIGABLE"
+    }
+    else {
+        let label = document.getElementById('eligableLabel')
+        label.textContent = "NOT ELIGABLE"
+    }
+})
+
+
+
+
 
 function checkEligability(sat, act, scisem1grds, scisem2grds,) {
-if (scisem1grds.length < 4 || scisem2grds.length < 4) {
-    console.log("You need more science courses.")
-    eligable = false
+    if (scisem1grds.length < 4 || scisem2grds.length < 4) {
+        console.log("You need more science courses.")
+        eligable = false
+        return false;
+
+    }
+    // check for num of class minimums here
 }
-}
+
+
 
 /*
 Organization Structure:
